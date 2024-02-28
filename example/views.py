22488a523b9f714d87ee0b89465e42df2759f11e
Rebecca Sealfon <reseal05@gmail.com>
2024-02-28 08:13:45 -0500

    Create and point to the view on the app

--- example/views.py
+++ example/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  html = f'''
+     <html>
+       <body>
+         <h1>Hello World!</h1>
+         <p>Congratulations on getting your first Vercel app to run.</p>
+       </body>
+     </html>
+   '''
+  return HttpResponse(html)
